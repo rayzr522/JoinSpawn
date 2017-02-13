9d9ba5e31cc662ad3be83505a2bd81ed639c47bf
Rayzr522 <rayzr522@gmail.com>
2017-02-13 09:56:19 -0500

    Project 'JoinSpawn' generated
    Generated using CNLPluginArchetype and custom CLI project generator tool by Rayzr

---
+++ README.md
@@ -0,0 +1,4 @@
+# JoinSpawn
+Teleports players to the spawn of their current world when they join.
+
+*Note: This README is a pre-generated stub*
---
+++ src/main/java/com/rayzr522/joinspawn/JoinSpawn.java
@@ -0,0 +1,15 @@
+package com.rayzr522.joinspawn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Rayzr
+ */
+public class JoinSpawn extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
