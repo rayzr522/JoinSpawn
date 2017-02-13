275e3514eb746dbe02b1bc47efc5646fed675dbb
Rayzr522 <rayzr522@gmail.com>
2017-02-13 10:03:26 -0500

    Done :D

--- src/main/java/com/rayzr522/joinspawn/JoinSpawn.java
+++ src/main/java/com/rayzr522/joinspawn/JoinSpawn.java
@@ -1,15 +1,20 @@
 package com.rayzr522.joinspawn;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-/**
- * @author Rayzr
- */
-public class JoinSpawn extends JavaPlugin {
+public class JoinSpawn extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
 
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        e.getPlayer().teleport(e.getPlayer().getLocation().getWorld().getSpawnLocation());
     }
 
 }
